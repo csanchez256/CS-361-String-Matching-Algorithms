bb2285fd6e6511d96c05da552e80e5d6d786696a
saucy <csanchez505@gmail.com>
2016-11-22 21:10:49 -0700

    added things to the script

--- testScript.py
+++ testScript.py
@@ -1,29 +1,36 @@
 #!/usr/bin/env python
 
+import sys
 import os
 import subprocess
 
-runtimes = []
+inputFile = sys.argv[1]
 
-trials = 10
-
-for n in range(trials):
+def runSimulation():
 	
-	os.system("java stringMatching STRING.input")
+	runtimes = []
+
+	trials = 10
+
+	for n in range(trials):
 	
-	output = subprocess.check_output("java stringMatching STRING.input", shell = True)
-	#parse the output from command prompt
-	for row in output.split('\n'):
-		if ': ' in row:
-			key, value = row.split(': ')
-			#result[key.strip('Total Runtime')] = value.strip('nano seconds')
-         		x = value.strip('nano seconds')
-                        num = int(x)
-			runtimes.append(num)		              			
+		os.system("java stringMatching STRING.input")
+	
+		output = subprocess.check_output("java stringMatching STRING.input", shell = True)
+		#parse the output from command prompt
+		for row in output.split('\n'):
+			if ': ' in row:
+				key, value = row.split(': ')
+				#result[key.strip('Total Runtime')] = value.strip('nano seconds')
+         			x = value.strip('nano seconds')
+                        	num = int(x)
+				runtimes.append(num)		              			
 
 
-# Loops through list, and sums each element 
-print "=================================="
-print "Average runtime is " , ( sum( int(n) for n in runtimes) ) / trials , " ns"
-print "=================================="
+	# Loops through list, and sums each element 
+	print "=================================="
+	print "Average runtime is " , ( sum( int(n) for n in runtimes) ) / trials , " ns"
+	print "=================================="
 
+
+runSimulation()
