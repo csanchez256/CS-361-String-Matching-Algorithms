7d5120ffc86ab68820364d6a70b12f7c51e11de7
saucy <csanchez505@gmail.com>
2016-11-28 00:58:30 -0700

    rolling hash doesn't work right

--- RabinKarp.java
+++ RabinKarp.java
@@ -128,10 +128,13 @@ public class RabinKarp {
 	private static void rabinKarp(int n, int m, ArrayList<Character> stringInput,
 			ArrayList<Character> patternInput) {
 		int occurances = 0;
-		int h = 0;
+		int previousChar = 0;
+		int lastCharInSubstring = 0;
+		int textHash = 0;
+//		int h = 0;
 
-		int patternHash = hash(patternInput.subList(0, m), h);
-		int textHash = hash(stringInput.subList(0, m), h);
+		int patternHash = hash(patternInput.subList(0, m), previousChar, textHash, lastCharInSubstring);
+		textHash = hash(stringInput.subList(0, m), previousChar, textHash, lastCharInSubstring );
 
 		
 		for (int i = 0; i < (n - m + 1); i++){
@@ -141,23 +144,38 @@ public class RabinKarp {
                     occurances++;					
 				}
 			}
-			textHash = (hash(stringInput.subList(i + 1, i + m + 1 ), h ) );
+			
+		    previousChar = stringInput.get(i);
+		    lastCharInSubstring = stringInput.get(i + m);
+			textHash = hash(stringInput.subList(i + 1, i + m + 1 ), previousChar, textHash,lastCharInSubstring );
+			
 		}
 		
 		
+		textHash = hashShift(textHash);
 		
 		if( occurances > 0 ) System.out.println("Number of occurances is " + occurances);
 		else System.out.println("Pattern was not found");
 	}
 	
-	private static int hash(List<Character> list, int h){
+	private static int hash(List<Character> list, int previousChar, int oldHash, int lastCharInSubstring){
 		int x = list.size();
 		int exp = x - 1;
 		int prime = 101;
+		int h = 0;
 		
-		for (int i = 0; i < x; i++){
-			h += (list.get(i)) * (Math.pow(prime, exp));
-			exp--;
+//		 int firstChar = (int) (( list.get(0) ) *(Math.pow(prime, exp) ));
+		//System.out.println("Previous char" + previousChar);
+		
+		if (previousChar == 0) {
+			for (int i = 0; i < x; i++) {
+				h += (list.get(i)) * (Math.pow(prime, exp));
+				exp--;
+			}
+		}
+		//Compute hash of next substring
+		else{
+			h = ( prime * (oldHash - (previousChar) ) ) + (lastCharInSubstring);
 		}
 		
 
@@ -166,6 +184,10 @@ public class RabinKarp {
 	}
 	
 	private static int hashShift(int oldHash){
+		
+////      base   old hash    old 'a'         new 'a'
+//hash("bra") = [101 × (999,509 - (97 × 1012))] + (97 × 1010) = 1,011,309
+		
 		int prime = 101;
 		return 0;
 	}
